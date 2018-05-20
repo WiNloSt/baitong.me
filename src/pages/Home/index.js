38d5dd6d347b415fbea3b966694f4ee421ced29d
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-05-20 21:56:19 +0700

    :iphone: Responsive work experience

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { FirstSection } from './FirstSection'
 import { SecondSection } from './SecondSection'
 import circle from './assets/circle.png'
 import line from './assets/line.png'
 
-const circleStyle = `
-::before {
-  content: '';
-  display: block;
-  width: 24px;
-  height: 24px;
-  position: absolute;
-  background-image: url(${circle});
-}
+const circleStyle = css`
+  ::before {
+    content: '';
+    display: block;
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    background-image: url(${circle});
+  }
 `
 
-const WorkExperience = styled.div`
-  position: relative;
-
-  ${circleStyle};
-
-  ::before,
-  ::after {
-    transform: translateX(${props => (props.right ? '-50%' : '50%')});
-    ${props => (props.right ? 'left: 0;' : 'right: 0;')};
-  }
-
+const lineStyle = css`
   &:not(:last-child)::after {
     content: '';
     display: block;
@@ -41,14 +31,77 @@ const WorkExperience = styled.div`
   }
 `
 
+const workExperienceStyle = css`
+  max-width: 500px;
+  margin: auto;
+`
+
+const WorkExperienceRight = styled.div`
+  ${workExperienceStyle};
+  ${circleStyle};
+  ${lineStyle};
+
+  position: relative;
+
+  ::before,
+  ::after {
+    transform: translateX(-50%);
+    left: 2rem;
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 50%;
+    max-width: unset;
+    margin-left: auto;
+    margin-right: unset;
+
+    ::before,
+    ::after {
+      left: 0rem;
+    }
+  }
+`
+const WorkExperienceLeft = styled.div`
+  ${workExperienceStyle};
+  ${circleStyle};
+  ${lineStyle};
+
+  position: relative;
+  padding-right: 1rem;
+  padding-left: 4rem;
+
+  ::before,
+  ::after {
+    transform: translateX(-50%);
+    left: 2rem;
+    right: unset;
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 50%;
+    max-width: unset;
+    margin-left: unset;
+    text-align: right;
+    padding-right: 4rem;
+    padding-left: 1rem;
+
+    ::before,
+    ::after {
+      transform: translateX(50%);
+      left: unset;
+      right: 0;
+    }
+  }
+`
+
 export default () => (
   <React.Fragment>
     <FirstSection />
     <SecondSection />
     <div className="mb5">
       <div className="container">
-        <h2 className="tc normal f4 mb4">WORK EXPERIENCES</h2>
-        <WorkExperience right className="w-50 ml-auto pv3 pl5 pr3">
+        <h2 className="tc normal f4 mt5 mb4">WORK EXPERIENCES</h2>
+        <WorkExperienceRight className="pv3 pl5 pr3">
           <h2 className="f5 normal ma0">PRODUCER</h2>
           <p className="f5 mt0">2015 - 2018 at Teapot Studio</p>
           <p className="text-grey">
@@ -63,15 +116,15 @@ export default () => (
             - Coordinate between different internal teams to ensure high work quality and
             consistency.
           </p>
-        </WorkExperience>
-        <WorkExperience className="w-50 pv3 pr5 pl3 tr">
+        </WorkExperienceRight>
+        <WorkExperienceLeft className="pv3">
           <h2 className="f5 normal ma0">2D Arkavis</h2>
           <p className="f5 mt0">2014 - 2015 at Teapot Studio</p>
           <p className="text-grey">- Produce high quality artworks following UI/UX standards.</p>
           <p className="text-grey mb0">
             - Create and deliver artworks to customers according to requirements.
           </p>
-        </WorkExperience>
+        </WorkExperienceLeft>
       </div>
     </div>
   </React.Fragment>
