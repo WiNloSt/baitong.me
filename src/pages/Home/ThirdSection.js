a2a9463050f1168c46761ba7a45864422f9a774e
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-05-27 00:32:27 +0700

    :construction: wip

--- src/pages/Home/ThirdSection.js
+++ src/pages/Home/ThirdSection.js
@@ -5,28 +5,19 @@ import circle from './assets/circle.png'
 import line from './assets/line.png'
 
 const circleStyle = css`
-  ::before {
-    content: '';
-    display: block;
-    width: 24px;
-    height: 24px;
-    position: absolute;
-    background-image: url(${circle});
-  }
+  width: 24px;
+  height: 24px;
+  background-image: url(${circle});
 `
 
 const lineStyle = css`
-  &:not(:last-child)::after {
-    content: '';
-    display: block;
-    position: absolute;
-    width: 4px;
-    top: 38px;
-    bottom: -16px;
-    background-image: url(${line});
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
+  position: absolute;
+  width: 4px;
+  top: 0;
+  bottom: 0;
+  background-image: url(${line});
+  background-repeat: no-repeat;
+  background-size: cover;
 `
 
 const workExperienceStyle = css`
@@ -36,9 +27,6 @@ const workExperienceStyle = css`
 
 const WorkExperienceRight = styled.div`
   ${workExperienceStyle};
-  ${circleStyle};
-  ${lineStyle};
-
   position: relative;
 
   ::before,
@@ -61,8 +49,6 @@ const WorkExperienceRight = styled.div`
 `
 const WorkExperienceLeft = styled.div`
   ${workExperienceStyle};
-  ${circleStyle};
-  ${lineStyle};
 
   position: relative;
   padding-right: 1rem;
@@ -92,33 +78,57 @@ const WorkExperienceLeft = styled.div`
   }
 `
 
+const Circle = styled.div`
+  ${circleStyle};
+
+  vertical-align: top;
+
+  ${props => (props.left ? 'transform: translateX(50%)' : '')};
+  ${props => (props.right ? 'transform: translateX(-50%)' : '')};
+`
+
+const Line = styled.div`
+  ${lineStyle};
+
+top: 24px;
+  transform: translateX(-50%);
+`
+
 export const ThirdSection = () => (
   <div className="mb5">
     <div className="container">
       <h2 className="tc normal f4 mt5 mb4">WORK EXPERIENCES</h2>
-      <WorkExperienceRight className="pv3 pl5 pr3">
-        <h2 className="f5 normal ma0">PRODUCER</h2>
-        <p className="f5 mt0">2015 - 2018 at Teapot Studio</p>
-        <p className="text-grey">
-          - Manage project from conception through deployment, and act as both a customer partnet
-          and an internal team leader.
-        </p>
-        <p className="text-grey">
-          - Manage daily project assignments, provide team direction and lead team meeting to
-          complete projects with clear objectives, agendas and outcomes.
-        </p>
-        <p className="text-grey mb0">
-          - Coordinate between different internal teams to ensure high work quality and consistency.
-        </p>
-      </WorkExperienceRight>
-      <WorkExperienceLeft className="pv3">
-        <h2 className="f5 normal ma0">2D Arkavis</h2>
-        <p className="f5 mt0">2014 - 2015 at Teapot Studio</p>
-        <p className="text-grey">- Produce high quality artworks following UI/UX standards.</p>
-        <p className="text-grey mb0">
-          - Create and deliver artworks to customers according to requirements.
-        </p>
-      </WorkExperienceLeft>
+      <div className="tr relative">
+        <Circle left className="dib" />
+        <Line left className="dib" />
+        <WorkExperienceRight className="dib pl5 pr3 tl">
+          <h2 className="f5 normal ma0">PRODUCER</h2>
+          <p className="f5 mt0">2015 - 2018 at Teapot Studio</p>
+          <p className="text-grey">
+            - Manage project from conception through deployment, and act as both a customer partnet
+            and an internal team leader.
+          </p>
+          <p className="text-grey">
+            - Manage daily project assignments, provide team direction and lead team meeting to
+            complete projects with clear objectives, agendas and outcomes.
+          </p>
+          <p className="text-grey mb0">
+            - Coordinate between different internal teams to ensure high work quality and
+            consistency.
+          </p>
+        </WorkExperienceRight>
+      </div>
+      <div>
+        <WorkExperienceLeft className="dib">
+          <h2 className="f5 normal ma0">2D Arkavis</h2>
+          <p className="f5 mt0">2014 - 2015 at Teapot Studio</p>
+          <p className="text-grey">- Produce high quality artworks following UI/UX standards.</p>
+          <p className="text-grey mb0">
+            - Create and deliver artworks to customers according to requirements.
+          </p>
+        </WorkExperienceLeft>
+        <Circle right className="dib" />
+      </div>
     </div>
   </div>
 )
