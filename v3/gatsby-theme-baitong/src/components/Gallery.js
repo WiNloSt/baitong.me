5fa1d215440b75c84b5cf373bfbbfe1a29218b46
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2021-05-24 14:45:36 +0700

    🐛 Fix SVG image not showing

--- v3/gatsby-theme-baitong/src/components/Gallery.js
+++ v3/gatsby-theme-baitong/src/components/Gallery.js
@@ -16,10 +16,18 @@ export function Gallery() {
             fluid(maxWidth: 600) {
               ...GatsbyContentfulFluid_withWebp
             }
+            file {
+              url
+              contentType
+            }
           }
           image {
             fluid(maxWidth: 1200) {
               ...GatsbyContentfulFluid_withWebp
+            }
+            file {
+              url
+              contentType
             }
           }
         }
@@ -46,7 +54,10 @@ export function Gallery() {
     <Grid>
       <Modal open={open} closeModal={closeModal}>
         {selectedItemIndex > -1 && (
-          <Img fluid={allContentfulPortfolio.nodes[selectedItemIndex].image.fluid} />
+          <Image
+            image={allContentfulPortfolio.nodes[selectedItemIndex].image}
+            alt={allContentfulPortfolio.nodes[selectedItemIndex].altText}
+          />
         )}
       </Modal>
       {allContentfulPortfolio.nodes.map((node, index) => {
@@ -57,10 +68,18 @@ export function Gallery() {
               handleClick()
               setSelectedItemIndex(index)
             }}>
-            <Img fluid={node.thumbnail.fluid} />
+            <Image image={node.thumbnail} alt={`${node.altText} thumbnail`} />
           </Grid.Item>
         )
       })}
     </Grid>
   )
+}
+
+function Image({ image, alt }) {
+  if (image.file.contentType === 'image/svg+xml') {
+    return <img style={{ width: '100%' }} src={image.file.url} alt={alt} />
+  } else {
+    return <Img fluid={image.fluid} alt={alt} />
+  }
 }
